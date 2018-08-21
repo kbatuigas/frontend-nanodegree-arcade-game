6eb844281967b384b213fb53d6008b62fecb51d7
kbatuigas <kbatuigas@gmail.com>
2018-08-20 20:08:33 -0700

    Create separate file for class declarations, declare parent and player subclass
    classes.js houses parent class Entity and subclass Player

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/*
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -30,7 +31,9 @@ Enemy.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+*/
 
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
